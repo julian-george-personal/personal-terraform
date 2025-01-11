b913a9f12a253c0c7d02143b0fe9daf62332ce4f
Julian George <juliancgeorge@gmail.com>
2025-01-11 15:49:12 -0500

    no awsvps

--- smart-guitar-chords/smart-guitar-chords.tf
+++ smart-guitar-chords/smart-guitar-chords.tf
@@ -32,7 +32,6 @@ resource "aws_iam_role_policy_attachment" "chords-ecs-perms" {
 resource "aws_ecs_task_definition" "chords-task" {
   family = local.app_name
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
   cpu = 256
   memory = 512 
   task_role_arn = aws_iam_role.chords-ecs-role.arn
