b0fb012d1d78d8b117473bb09bc9a080cb933957
Julian George <juliancgeorge@gmail.com>
2025-01-11 14:59:31 -0500

    enable

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_route53_zone" "primary" {
   name = "juliangeorge.net"
 }
 
-# module "smart-guitar-chords" {
-#   source = "./smart-guitar-chords"
-#   is_enabled = true
-# }
+module "smart-guitar-chords" {
+  source = "./smart-guitar-chords"
+  is_enabled = true
+}
