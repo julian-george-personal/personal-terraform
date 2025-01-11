9572f1e091c87ceba7195db1e27e572365d634ce
Julian George <juliancgeorge@gmail.com>
2025-01-11 16:08:00 -0500

    fixing some syntax

--- ecs-application/ecs-application.tf
+++ ecs-application/ecs-application.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role" "ecs-task-role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-attachment" {
-  role = aws_iam_role.chords-ecs-role.name
+  role = aws_iam_role.ecs-task-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
@@ -43,8 +43,8 @@ resource "aws_ecs_task_definition" "ecs-task" {
   network_mode             = "awsvpc"
   cpu = 256
   memory = 512 
-  task_role_arn = aws_iam_role.chords-ecs-role.arn
-  execution_role_arn = aws_iam_role.chords-ecs-role.arn
+  task_role_arn = aws_iam_role.ecs-task-role.arn
+  execution_role_arn = aws_iam_role.ecs-task-role.arn
   container_definitions = jsonencode([
     {
       name      = var.app_name
@@ -63,7 +63,7 @@ resource "aws_ecs_service" "ecs-service" {
   desired_count = 1
   launch_type   = "FARGATE"
   network_configuration {
-    subnets = aws_subnet.ecs-subnet.id
+    subnets = [aws_subnet.ecs-subnet.id]
     assign_public_ip = true
   }
 }
