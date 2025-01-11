5f809c66d407de48e4b7a4b52b582579db535c8e
Julian George <juliancgeorge@gmail.com>
2025-01-11 17:04:05 -0500

    remove ecs

--- main.tf
+++ main.tf
@@ -5,9 +5,3 @@ provider "aws" {
 resource "aws_route53_zone" "primary" {
   name = "juliangeorge.net"
 }
-
-module "smart-guitar-chords" {
-  source = "./ecs-application"
-  app_name="smart-guitar-chords"
-  count = 1
-}
