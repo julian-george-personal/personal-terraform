b5581cc988ebcf2b240b2a173227684b430c6fad
Julian George <juliancgeorge@gmail.com>
2026-04-13 19:08:44 -0400

    fix cloudfront

--- aws-s3-application/aws-s3-application.tf
+++ aws-s3-application/aws-s3-application.tf
@@ -84,7 +84,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
