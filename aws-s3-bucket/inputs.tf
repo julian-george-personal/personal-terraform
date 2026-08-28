f96c99eb93c755c9cc0287d0ede75c9d8137d3a0
Julian George <juliancgeorge@gmail.com>
2026-08-27 22:35:29 -0400

    remove hedge-trimmer data bucket, add force_destroy to s3 module

--- aws-s3-bucket/inputs.tf
+++ aws-s3-bucket/inputs.tf
@@ -6,3 +6,8 @@ variable "versioning_enabled" {
   type    = bool
   default = false
 }
+
+variable "force_destroy" {
+  type    = bool
+  default = false
+}
--- main.tf
+++ main.tf
@@ -10,10 +10,9 @@ terraform {
 }
 
 locals {
-  personal_domain_name           = "juliangeorge.net"
-  static_sites_bucket_name       = "static-sites-juliangeorge"
-  lambda_functions_bucket_name   = "lambda-functions-juliangeorge"
-  hedge_trimmer_data_bucket_name = "hedge-trimmer-juliangeorge"
+  personal_domain_name         = "juliangeorge.net"
+  static_sites_bucket_name     = "static-sites-juliangeorge"
+  lambda_functions_bucket_name = "lambda-functions-juliangeorge"
 }
 
 provider "aws" {
@@ -46,12 +45,6 @@ resource "aws_s3_bucket" "static-sites" {
 module "lambda-functions-bucket" {
   source             = "./aws-s3-bucket"
   bucket_name        = local.lambda_functions_bucket_name
-  versioning_enabled = true
-}
-
-module "hedge-trimmer-data-bucket" {
-  source             = "./aws-s3-bucket"
-  bucket_name        = local.hedge_trimmer_data_bucket_name
   versioning_enabled = true
 }
 
